4c97dc8984247360b1a44b42054a4dca4845575f
Keegan Donley <kdonley@pinpoint.com>
2021-08-03 12:47:16 -0500

    use new prebuilt component for home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,87 +2,29 @@ import {
 	fetchSite,
 	Site,
 	Entry,
-	Page,
-	Header,
-	Subscribe,
-	splitEntries,
-	Latest,
 	ChangelogCard,
-	Statistic,
-	Recent,
-	Footer,
-	Copyright,
-	Logo,
-	Social,
+	Prebuilt,
 } from '@pinpt/react';
 import { useRouter } from 'next/dist/client/router';
 import config from '../pinpoint.config';
-
 interface HomeProps {
 	site: Site;
-	changelogs: Entry[];
+	entries: Entry[];
 }
 
-const Card = ({ entry }: { entry: Entry }) => {
+
+export default function Home(props: HomeProps) {
+	const { site, entries } = props;
 	const router = useRouter();
 
 	return (
-		<ChangelogCard.Container
-			imageUrl={entry.cover_image}
-			title={<ChangelogCard.Title title={entry.title} />}
-			date={<ChangelogCard.Date ts={entry.publishedAt} />}
-			description={<ChangelogCard.Description description={entry.headline} />}
-			statistics={<Statistic.Bar claps={0} views={0} />}
-			button={<ChangelogCard.ReadButton onClick={() => router.push(`/entry/${entry.id}`)} />}
-		/>
-	);
-};
-
-export default function Home(props: HomeProps) {
-	const { site, changelogs } = props;
-	const { latest, recent } = splitEntries(changelogs, 2);
-
-	return (
-		<Page.Dashboard
-			header={<Header title={`${site.name} Changelog`} description={site.theme.description} subscribe={<Subscribe />} />}
-			latest={
-				latest.length > 0 ? (
-					<Latest>
-						{latest.map((entry) => {
-							return <Card key={entry.id} entry={entry} />;
-						})}
-					</Latest>
-				) : undefined
-			}
-			recent={
-				recent.length > 0 ? (
-					<Recent>
-						{recent.map((entry) => {
-							return <Card key={entry.id} entry={entry} />;
-						})}
-					</Recent>
-				) : undefined
-			}
-			footer={
-				<Footer
-					copyright={
-						<Copyright
-							text={site.theme.copyright}
-							logo={<Logo src={site.logoUrl} href={site.theme.logoLink} />}
-						/>
-					}
-					subscribe={<Subscribe />}
-					social={
-						<Social.Bar>
-							{site.theme.social?.facebook && <Social.Facebook href={site.theme.social.facebook} />}
-							{site.theme.social?.instagram && <Social.Instagram href={site.theme.social.instagram} />}
-							{site.theme.social?.linkedin && <Social.LinkedIn href={site.theme.social.linkedin} />}
-							{site.theme.social?.github && <Social.Github href={site.theme.social.github} />}
-							{site.theme.social?.twitter && <Social.Twitter href={site.theme.social.twitter} />}
-						</Social.Bar>
-					}
-				/>
-			}
+		<Prebuilt.Home
+			entries={entries}
+			site={site}
+			latestCount={2}
+			renderCardButton={(entry) => (
+				<ChangelogCard.ReadButton onClick={() => router.push(`/entry/${entry.id}`)} />
+			)}
 		/>
 	);
 }
@@ -92,7 +34,7 @@ export async function getStaticProps() {
 	return {
 		props: {
 			site,
-			changelogs,
+			entries: changelogs,
 		},
 	};
 }
