df46c7cd3801504c4a6c36b76c1991c481cdfeda
Keegan Donley <kdonley@pinpoint.com>
2021-08-03 15:23:37 -0500

    update to tailwind compiled version of @pinpt/react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
-import '@pinpt/react/base.css';
+import '@pinpt/react/dist/base.css';
 import '../theme.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
