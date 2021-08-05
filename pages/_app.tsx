073bcfdb9bd28540fc945da0c8c14ecadf91db14
Keegan Donley <kdonley@pinpoint.com>
2021-08-05 15:23:19 -0500

    update to use entry styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import '@pinpt/react/dist/base.css';
+import '@pinpt/react/dist/entry.css';
 import '@pinpt/react/themes/default.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
