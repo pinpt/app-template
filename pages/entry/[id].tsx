4192dabb728a0cbc931b2a503d330ba35541e358
Keegan Donley <kdonley@pinpoint.com>
2021-08-06 15:48:53 -0500

    wire up clap

--- pages/entry/[id].tsx
+++ pages/entry/[id].tsx
@@ -1,10 +1,34 @@
-import { Entry, fetchContent, fetchSite, Prebuilt, Site } from '@pinpt/react';
+import { Entry, fetchContent, fetchSite, Prebuilt, Site, createClap, fetchAnalytics } from '@pinpt/react';
+import { useCallback, useState } from 'react';
 import config from '../../pinpoint.config';
 
-export default function EntryPage(props: { entry: Entry; site: Site }) {
-	const { entry, site } = props;
+export default function EntryPage(props: { entry: Entry; site: Site; analytics: { claps: number } }) {
+	const { entry, site, analytics } = props;
+	const [sessionCount, setSessionCount] = useState(1);
+	const [totalCount, setTotalCount] = useState(analytics.claps);
+	const [maxed, setMaxed] = useState(false);
 
-	return <Prebuilt.Entry entry={entry} site={site} />;
+	const onClap = useCallback(
+		async (siteId: string, entryId: string) => {
+			if (!maxed) {
+				const res = await createClap(siteId, entryId);
+				setSessionCount(res.sessionCount);
+				setTotalCount(res.count);
+				setMaxed(res.max);
+			}
+		},
+		[maxed]
+	);
+
+	return (
+		<Prebuilt.Entry
+			entry={entry}
+			site={site}
+			onClap={onClap}
+			clapCount={totalCount}
+			sessionClapCount={sessionCount}
+		/>
+	);
 }
 
 export async function getStaticPaths() {
@@ -23,10 +47,13 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: { params: { id: string; title: string } }) {
 	const [entry, siteData] = await Promise.all([fetchContent(params.id), fetchSite(config.slug)]);
+	const analytics = await fetchAnalytics(siteData.site.id, [entry.id]);
+
 	return {
 		props: {
 			entry,
 			site: siteData.site,
+			analytics: analytics?.[entry.id],
 		},
 	};
 }
