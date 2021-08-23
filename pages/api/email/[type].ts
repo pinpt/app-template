d74290c9eb1fc59bf4b77694b40820c601be126f
Jeff Haynie <jhaynie@pinpoint.com>
2021-08-23 16:52:41 -0500

    Bump email and catch error in route

--- pages/api/email/[type].ts
+++ pages/api/email/[type].ts
@@ -3,18 +3,23 @@ import { generate } from '@pinpt/email';
 
 export default function handler(req: NextApiRequest, resp: NextApiResponse) {
 	if (req.method !== 'POST') {
-		resp.status(400).json({ success: false, error: 'Invalid request method' });
+		resp.status(400).json({ success: false, message: 'Invalid request method' });
 		return;
 	}
 	if (typeof req.body !== 'object') {
-		resp.status(400).json({ success: false, error: 'Invalid request body' });
+		resp.status(400).json({ success: false, message: 'Invalid request body' });
 		return;
 	}
-	const data = { ...req.body };
-	const { type } = req.query;
-	// by default, we'll pass the data coming in to the template engine to generate the default
-	// email body for this content type. if you want to use your own template, you can just return
-	// the HTML here using the incoming data object.
-	const html = generate(type as string, data);
-	resp.json({ success: true, html });
+	try {
+		const data = { ...req.body };
+		const { type } = req.query;
+		// by default, we'll pass the data coming in to the template engine to generate the default
+		// email body for this content type. if you want to use your own template, you can just return
+		// the HTML here using the incoming data object.
+		const html = generate(type as string, data);
+		resp.json({ success: true, html });
+	} catch (ex) {
+		console.error(ex);
+		resp.status(500).json({ success: false, message: ex.message });
+	}
 }
