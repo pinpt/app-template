fd685d2dc83112509ca53743eafdfb5e57e440b7
Keegan Donley <kdonley@pinpoint.com>
2021-11-16 11:15:14 -0600

    use default title and description if empty string

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -17,7 +17,7 @@ const Roadmap = ({ site, roadmap }: { site: ISite; roadmap: PublishedRoadmapResp
 			<PrebuiltRoadmap
 				site={site}
 				roadmap={roadmap}
-				renderHeader={(site) => <Header site={site} title={roadmap.title} description={roadmap.description} />}
+				renderHeader={(site) => <Header site={site} title={roadmap.title || undefined} description={roadmap.description || undefined} />}
 				renderFooter={(site) => <Footer site={site} />}
 			/>
 		</div>
